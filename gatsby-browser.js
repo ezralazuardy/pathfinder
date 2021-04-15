f56b29e28489d801909aa1c2b6b9c5f87ffbba9a
Ezra Lazuardy <ezralucio@gmail.com>
2021-04-16 03:02:28 +0700

    Update jQuery version

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,7 @@ import { withPrefix } from "gatsby-link"
 import "./src/styles/pathfinder.min.css"
 
 const scripts = [
-	"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js",
+	"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js",
 	"https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js",
 	withPrefix("js/graph.min.js"),
 	withPrefix("js/pathfinder.min.js")
